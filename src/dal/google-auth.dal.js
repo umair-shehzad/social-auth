c61f0d8b34ab84807840f809f93116e6a3b83be9
Umair Shehzad <umairshehzad247365@gmail.com>
2023-04-09 16:54:08 +0500

    Initial Commit

---
+++ src/dal/google-auth.dal.js
@@ -0,0 +1,31 @@
+const User = require('./models/user.model');
+
+const googleAuthDal = {
+  registerWithGoogle: async (oauthUser) => {
+    const isUserExists = await User.findOne({
+      accountId: oauthUser.id,
+      provider: oauthUser.provider,
+    });
+    if (isUserExists) {
+      const failure = {
+        message: 'User already Registered.',
+      };
+      return { failure };
+    }
+
+    const user = new User({
+      accountId: oauthUser.id,
+      name: oauthUser.displayName,
+      provider: oauthUser.provider,
+      email: oauthUser.emails[0].value, //optional - storing it as extra info
+      photoURL: oauthUser.photos[0].value, //optional
+    });
+    await user.save();
+    const success = {
+      message: 'User Registered.',
+    };
+    return { success };
+  },
+};
+
+module.exports = googleAuthDal;
